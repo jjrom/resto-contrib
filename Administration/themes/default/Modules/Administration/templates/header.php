9e76ab39b6c29e646e568103e356c586a3a351e1
rmourembles <rmourembles@users.noreply.github.com>
2014-10-31 16:18:15 +0100

    minor bugs correction

--- Administration/themes/default/Modules/Administration/templates/header.php
+++ Administration/themes/default/Modules/Administration/templates/header.php
@@ -1,7 +1,25 @@
 <header>
-    <span id="logo"><a title="<?php echo $self->context->dictionary->translate('_home'); ?>" href="<?php echo $self->context->baseUrl ?>">resto</a></span>
+    
+    <span id="logo">
+        <a title="<?php echo $self->context->dictionary->translate('_home'); ?>" href="<?php echo $self->context->baseUrl ?>">resto</a>
+    </span>
+    
+    <span style="margin-left: 20px; margin-top: 15px; float: left; color: white;">
+        <?php
+        $i = 0;
+        $url = $self->context->baseUrl . 'administration';
+        echo '<a href="' . $url . '">administration</a>';
+        while ($i < sizeof($self->segments)) {
+            $url = $url . '/' . $self->segments[$i];
+            echo ' > <a href="' . $url . '">' . $self->segments[$i] . '</a>';
+            ++$i;
+        }
+        ?>
+    </span>
+    
     <nav>
         <ul class="no-bullet">
+            
             <?php if (!isset($_noSearchBar)) { ?>
             <li><span class="resto-search"><input type="text" id="search" name="q" placeholder="<?php echo $self->context->dictionary->translate('_menu_search'); ?>" value="<?php echo isset($self->context->query['q']) ? $self->context->query['q'] : ''; ?>"></span><li>
             <?php
@@ -10,9 +28,6 @@
             }
             ?>
             <?php } ?>
-            <li title="<?php echo $self->context->dictionary->translate('_menu_shareOn', 'Facebook'); ?>" class="fa fa-facebook link shareOnFacebook"></li>
-            <li title="<?php echo $self->context->dictionary->translate('_menu_shareOn', 'Twitter'); ?>" class="fa fa-twitter link shareOnTwitter"></li>
-            <li title="<?php echo $self->context->dictionary->translate('_menu_viewCart'); ?>" class="fa fa-shopping-cart link"></li>
             <li></li>
             <?php if ($self->context->user->profile['userid'] === -1) { ?>
             <li title="<?php echo $self->context->dictionary->translate('_menu_connexion'); ?>" class="link viewUserPanel"><?php echo $self->context->dictionary->translate('_menu_connexion'); ?></li>
@@ -22,21 +37,5 @@
         </ul>
     </nav>
 </header>
-<div id="breadcrumb">
-    <a href="<?php echo $self->context->baseUrl?>">home</a>
-<?php if ($self->context->path !== '') {
-    $splitted = explode('/', $self->context->path);
-    if ($splitted[0] === 'collections' || (isset($splitted[1]) && $splitted[1] === 'collections')) {
-        echo ' > <a href="' . $self->context->baseUrl . 'collections">collections</a>';
-    }
-    if ($splitted[0] === 'api' && isset($splitted[2])) {
-        echo ' > <a href="' . $self->context->baseUrl . 'api/collections/' . $splitted[2] . '/search.html" >' . $splitted[2] . '</a>';
-    }
-    if ($splitted[0] === 'collections' && isset($splitted[2])) {
-        echo ' > <a href="' . $self->context->baseUrl . 'api/collections/' . $splitted[1] . '/search.html" >' . $splitted[1] . '</a>';
-        echo ' > <a href="' . $self->context->baseUrl . 'collections/' . $splitted[1] . '/' . $splitted[2] . '" >' . $splitted[2] . '</a>';
-    }
-}
-?>
-</div>
+
         

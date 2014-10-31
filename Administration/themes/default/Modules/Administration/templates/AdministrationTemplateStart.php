e0f64f52ffe6cf85fd3cbfcdb555e578151d86f7
rmourembles <rmourembles@users.noreply.github.com>
2014-10-31 16:15:47 +0100

    minor bugs correction

--- Administration/themes/default/Modules/Administration/templates/AdministrationTemplateStart.php
+++ Administration/themes/default/Modules/Administration/templates/AdministrationTemplateStart.php
@@ -5,7 +5,7 @@ $_noMap = true;
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en">
     <?php include 'head.php' ?>
-    <body>
+    <body style="overflow-x: hidden;">
         <!-- Header -->
         <?php include 'header.php' ?>
 
@@ -32,12 +32,12 @@ $_noMap = true;
                         </li>
                         <li>
 
-                            <a href="<?php echo $self->context->baseUrl . 'administration/users/creation'; ?>" class="button expand"><?php echo $self->context->dictionary->translate('_a_user_creation'); ?></a>
+                            <a href="<?php echo $self->context->baseUrl . 'administration/collections'; ?>" class="button expand"><?php echo $self->context->dictionary->translate('_a_collections_management'); ?></a>
 
                         </li>
                         <li>
 
-                            <a href="<?php echo $self->context->baseUrl . 'administration/collections'; ?>" class="button expand"><?php echo $self->context->dictionary->translate('_a_collections_management'); ?></a>
+                            <a href="<?php echo $self->context->baseUrl . 'administration/users/creation'; ?>" class="button expand"><?php echo $self->context->dictionary->translate('_a_user_creation'); ?></a>
 
                         </li>
                         <li>
@@ -52,5 +52,18 @@ $_noMap = true;
         </div>
         <!-- Footer -->
         <?php include 'footer.php' ?>
+        
+        <script type="text/javascript" >
+            $(document).ready(function() {
+                
+                 R.init({
+                    language: '<?php echo $self->context->dictionary->language; ?>',
+                    translation:<?php echo json_encode($self->context->dictionary->getTranslation()) ?>,
+                    restoUrl: '<?php echo $self->context->baseUrl ?>',
+                    ssoServices:<?php echo json_encode($self->context->config['ssoServices']) ?>,
+                    userProfile:<?php echo json_encode(!isset($_SESSION['profile']) ? array('userid' => -1) : array_merge($_SESSION['profile'], array())) ?> 
+                });
+            });
+        </script>
     </body>
 </html>

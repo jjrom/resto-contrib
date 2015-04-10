97409aae344198ba2c8682d14206cc34ac7d672f
jerome.gasperi@gmail.com <>
2015-04-10 20:58:27 +0200

    Correct configuration file

--- Administration/client/app/app.configuration.js
+++ Administration/client/app/app.configuration.js
@@ -22,9 +22,8 @@
 
 angular.module('administration')
         .constant('CONFIG', {
-            'restoURL': 'http://localhost/resto2/',
-            //'restoURL': 'http://resto.mapshup.com/2.0/',
-            'administrationEndpoint': 'administration/',
+            'restoURL': 'http://localhost/resto2',
+            'administrationEndpoint': '/administration',
             'offset': 55,
             'version': 0.1
         });

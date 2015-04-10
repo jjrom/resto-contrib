9330c54c3dcf3067df27d05d4487c79eb3dbb42a
rmouremb <remi.mourembles@gmail.com>
2015-04-10 15:44:07 +0200

    update folders architecture update readme

--- Administration/client/js/configuration.js
+++
@@ -1,7 +0,0 @@
-angular.module('administration')
-        .constant('CONFIG', {
-            'restoURL': 'http://localhost/resto2/',
-            'administrationEndpoint': 'administration/',
-            'offset': 55,
-            'version': 0.1
-        });

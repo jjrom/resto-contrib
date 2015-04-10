e054e514040d2da8fef3789fa9e6d1f1f2e94162
rmouremb <remi.mourembles@gmail.com>
2015-04-10 14:45:58 +0200

    error in configuration file...

--- Administration/client/js/configuration.js
+++ Administration/client/js/configuration.js
@@ -1,7 +1,6 @@
 angular.module('administration')
         .constant('CONFIG', {
             'restoURL': 'http://localhost/resto2/',
-            //'restoURL': 'http://resto.mapshup.com/2.0/',
             'administrationEndpoint': 'administration/',
             'offset': 55,
             'version': 0.1
